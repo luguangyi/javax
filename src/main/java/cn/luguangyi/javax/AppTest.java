549bb8dbff3f000c05f8925928fc4f3957e5eec3
卢光怡 <luguangyi@lattebank.com>
2018-04-10 11:07:51 +0800

    Init

---
+++ src/main/java/cn/luguangyi/javax/AppTest.java
@@ -0,0 +1,10 @@
+package cn.luguangyi.javax;
+
+/**
+ * Hello world!
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
