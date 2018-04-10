c1016de6b04c3f9f65dac56a990ef6d8b600b946
卢光怡 <luguangyi@lattebank.com>
2018-04-10 11:18:28 +0800

    Add develop branch.

--- src/main/java/cn/luguangyi/javax/AppTest.java
+++ src/main/java/cn/luguangyi/javax/AppTest.java
@@ -6,5 +6,6 @@ package cn.luguangyi.javax;
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Here is in develop branch.");
     }
 }
